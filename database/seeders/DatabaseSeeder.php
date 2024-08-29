6d4acbc70d96f77da0f30849e471ac915d710f1b
tqt97 <kutuanonline199@gmail.com>
2024-08-29 15:22:05 +0700

    List of Data: Routing, Controllers and Clients Demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Category::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
